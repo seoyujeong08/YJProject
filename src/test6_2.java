9ec514adfb5851c21b34a73929e74a2ddf543346
seyoujeong_YJ_STPJECT <dbswoakswk@naver.com>
2021-06-28 10:40:07 +0900

    과제 첫 업로드

---
+++ src/test6_2.java
@@ -0,0 +1,23 @@
+package test6;
+
+import java.util.Scanner;
+
+public class test6_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner myInput = new Scanner(System.in);
+
+		// while (true) {
+		System.out.println("이용 날짜를 입력해주세요. (ex 2021 06 05)");
+		int year, month, day;
+		year = myInput.nextInt();
+		month = myInput.nextInt();
+		day = myInput.nextInt();
+
+		int ticketA = 60000, ticketB = 56000, ticketC = 50000;
+
+		// }
+
+	}
+}
